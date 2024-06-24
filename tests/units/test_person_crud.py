077c89799c8944dde95a51f7f6266ab060ec5ab7
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-06-24 19:48:11 -0300

    refactor: refactoring tests

--- tests/units/test_person_crud.py
+++ tests/units/test_person_crud.py
@@ -4,47 +4,35 @@ from faker import Faker
 
 from src.database.conn import Connection
 from src.crud.persons_crud import PersonsCrud
-from src.utils.uuid import UUID
 
 
 class TestPersonCrud:
-
-    # Config Teardown
     @pytest.fixture(autouse=True)
     def setup_teardown(self):
         self.db_name = 'test.db'
         self.conn: Connection = Connection(self.db_name)
         self.conn.connect()
 
-        yield
-
-        self.conn.close()
-
-        if os.path.exists(self.db_name):
-            os.remove(self.db_name)
-
     @pytest.fixture(scope='class')
     def test_data(self):
         faker = Faker()
 
-        test_email = faker.email()
-        test_password = faker.password()
-        test_name = f'{faker.name()} {Faker().last_name()}'
+        test_data: dict = {}
 
-        return {
-            "name": test_name,
-            "email": test_email,
-            "password": test_password,
-        }
+        test_data['name'] = f'{faker.name()} {Faker().last_name()}'
+        test_data['email'] = faker.email()
+        test_data['password'] = faker.password()
+
+        return test_data
 
     def test_insert_person(self, test_data):
         crud: PersonsCrud = PersonsCrud(self.conn)
         is_created = crud.insert_person(test_data)
         assert is_created is True
 
-    def test_select_person_by_email(self, test_data):
+    def test_select_by_email(self, test_data):
         crud: PersonsCrud = PersonsCrud(self.conn)
         crud.insert_person(test_data)
-        
-        person = crud.select_person_by_email(test_data['email'])
-        assert isinstance(person, tuple)
+
+        person = crud.select_by_email(test_data['email'])
+        assert person is not None
--- tests/units/test_schemas.py
+++ tests/units/test_schemas.py
@@ -11,24 +11,25 @@ class TestPersonCreate:
     @pytest.fixture(scope='class')
     def test_data(self):
         faker = Faker()
+        test_data: dict = {}
 
-        test_email = faker.email()
-        test_password = faker.password()
-        test_name = f'{faker.name()} {Faker().last_name()}'
+        test_data['person_id'] = UUID().smaller_uuid()
+        test_data['name'] = f'{faker.name()} {Faker().last_name()}'
+        test_data['email'] = faker.email()
+        test_data['password'] = faker.password()
 
-        return {
-            "name": test_name,
-            "email": test_email,
-            "password": test_password,
-        }
+        return test_data
 
     @pytest.fixture(scope='class')
     def invalid_data(self):
-        return {
-            "name": 123,
-            "email": "invalid_email",
-            "password": "",
-        }
+        test_fail_data: dict = {}
+
+        test_fail_data['person_id'] = 'invalid_uuid'
+        test_fail_data['name'] = 123
+        test_fail_data['email'] = 'invalid_email'
+        test_fail_data['password'] = ''
+
+        return test_fail_data
 
     def test_valid_person(self, test_data):
         person = PersonCreate(**test_data)
