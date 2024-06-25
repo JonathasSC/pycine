ebbb86789facbf5f9b4d877980cc0bb0b35e6269
JonathasSC <jonathas.s.cardoso28@gmail.com>
2024-06-25 14:55:13 -0300

    feat: added movie schemas

---
+++ src/schemas/movie_schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, field_validator
+from typing import Optional
+
+
+class MovieBase(BaseModel):
+    movie_id: str
+
+
+class MovieCreate(MovieBase):
+    name: str
+    duration: str
+    synopsis: Optional[str]
+
+    @field_validator('name')
+    def validate_name(cls, value: str):
+        if not value.strip():
+            raise ValueError('Name must not be empty.')
+
+        if len(value) < 2:
+            raise ValueError('Name must be at least 2 characters long.')
+
+        return value
+
+    @field_validator('synopsis')
+    def validate_name(cls, value: str):
+        if not value.strip():
+            raise ValueError('Synopsis must not be empty.')
+
+        if len(value) < 2:
+            raise ValueError('Synopsis must be at least 2 characters long.')
+
+        return value
