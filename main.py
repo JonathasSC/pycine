959a6dc61710c517824f172f73de126dc2099419
JonathasSC <jonathas.s.cardoso28@gmail.com>
2024-07-03 16:55:03 -0300

    cleanup: removig Populate and Connection

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from src.views.home_view import HomeView
-from src.utils.populate import Populate
-from src.database.conn import Connection
 
 
 def main():
-    Connection().delete_data()
-    Connection().create_database()
-
-    Populate().populate_sessions()
     HomeView().start()
 
 
