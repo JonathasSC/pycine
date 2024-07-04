680b1388903968af52ceec373c352ca202ebf43b
JonathasSC <jonathas.s.cardoso28@gmail.com>
2024-07-04 13:56:48 -0300

    refactor: refactoring display table

--- src/utils/printer.py
+++ src/utils/printer.py
@@ -1,11 +1,13 @@
 from rich.console import Console
 from tabulate import tabulate
 from time import sleep
+from src.utils.terminal import Terminal
 
 
 class Printer:
     def __init__(self) -> None:
         self.console = Console()
+        self.terminal = Terminal()
 
     def generic(self, text: str, color: str = 'white', line: bool = False, timer: bool = False):
         if line:
@@ -40,39 +42,48 @@ class Printer:
             self.generic(f'[{index + 1}] - {item}')
 
     def display_table(self, headers: list, table_data: list, page: int = 1, per_page: int = 10):
-        try:
-            start_idx: int = (page - 1) * per_page
-            end_idx: int = start_idx + per_page
+        total_pages = (len(table_data) + per_page - 1) // per_page
 
-            paginated_data = table_data[start_idx:end_idx]
-            table = tabulate(paginated_data, headers=headers, tablefmt='grid')
-            print(table)
-            total_pages = (len(table_data) + per_page - 1) // per_page
+        while True:
+            self.terminal.clear()
+            try:
+                start_idx = (page - 1) * per_page
+                end_idx = start_idx + per_page
+                paginated_data = table_data[start_idx:end_idx]
 
-            while True:
-                user_input = input(
-                    f"Digite o número da página (1 - {total_pages}) ou 'q' para voltar: ").strip().lower()
+                table = tabulate(
+                    paginated_data,
+                    headers=headers,
+                    tablefmt='grid'
+                )
 
-                if user_input == 'q':
+                print(table)
+
+                page_input = self.validate_page(total_pages)
+
+                if page_input is None:
                     break
-
-                elif user_input.isdigit():
-                    page = int(user_input)
-                    if 1 <= page <= total_pages:
-                        start_idx = (page - 1) * per_page
-                        end_idx = start_idx + per_page
-                        paginated_data = table_data[start_idx:end_idx]
-                        table = tabulate(
-                            paginated_data, headers=headers, tablefmt='grid'
-                        )
-
-                        print(table)
-                    else:
-                        print(
-                            f"Página inválida. Digite um número entre 1 e {total_pages}.")
+                elif 1 <= page_input <= total_pages:
+                    page = page_input
                 else:
                     print(
-                        "Entrada inválida. Digite um número de página válido ou 'q' para sair.")
+                        f"Digite um número de página válido (1 - {total_pages}) ou '0' para voltar.")
 
-        except Exception as e:
-            print(f'Erro ao mostrar tabela: {e}')
+            except Exception as e:
+                print(f'Erro ao mostrar tabela: {e}')
+
+    def validate_page(self, total_pages):
+        while True:
+            try:
+                user_input = int(
+                    input(f"Digite o número da página (1 - {total_pages}) ou '0' para voltar: "))
+
+                if user_input == 0 or user_input not in range(1, total_pages + 1):
+                    return None
+                else:
+                    return user_input
+
+            except ValueError:
+                print("Entrada inválida. Por favor, digite um número válido.")
+            except Exception as e:
+                raise e
