42c789abc14eb778bbf370b9db78b1e3f807c0c3
JonathasSC <jonathas.s.cardoso28@gmail.com>
2024-07-24 13:44:22 -0300

    chore(rename): exceptions.py > handlers.py

---
+++ src/utils/handlers.py
@@ -0,0 +1,25 @@
+from pydantic import ValidationError
+from src.utils.printer import Printer
+from src.utils.terminal import Terminal
+
+
+class Handlers:
+    def __init__(self) -> None:
+        self.printer: Printer = Printer()
+        self.terminal: Terminal = Terminal()
+
+    def handle_validation_error(self, e: ValidationError):
+        for error in e.errors():
+            field = " -> ".join(map(str, error['loc']))
+            self.printer.error(f'Erro no campo {field}, tente novamente', True)
+        self.terminal.clear()
+
+    def handle_no_sessions_available(self):
+        self.terminal.clear()
+        self.printer.warning("Nenhuma sessão disponível.")
+        self.terminal.clear()
+
+    def handle_no_tickets(self):
+        self.terminal.clear()
+        self.printer.warning("Nenhum ticket comprado até o momento.")
+        self.terminal.clear()
