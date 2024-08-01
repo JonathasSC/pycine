5d0dad7efb42ea79733d39546b71276cde257348
JonathasSC <jonathas.s.cardoso28@gmail.com>
2024-08-01 15:03:03 -0300

    feat: implemented maximium room capacityfunction

--- src/utils/counters.py
+++ src/utils/counters.py
@@ -1,7 +1,12 @@
 from src.crud.rooms_crud import RoomsCrud
+from src.crud.seats_crud import SeatsCrud
 
 
-def room_dimensions(room_id: str):
+def maximum_room_capacity(room_id: str):
+    seats_count = SeatsCrud().count_seats_by_room_id(room_id)
     room: str = RoomsCrud().select_room_by_id(room_id)
     room_dimensions: int = room[2] * room[3]
-    return room_dimensions
+
+    if room_dimensions <= seats_count:
+        return True
+    return False
